ba301d766d0d14cf4b8639813886edb39536f827
bmlsj <gaga2353@gmail.com>
2022-08-03 13:58:57 +0900

    10. Method

--- prjTest/src/prjTest/Method_10.java
+++ prjTest/src/prjTest/Method_10.java
@@ -1,2 +1,44 @@
-package prjTest;public class Method_10 {
+package prjTest;
+
+public class Method_10 {
+
+    // 10. 메소드
+    // 함수 역할
+
+    // 1) 매개 변수x, 리턴x
+    public void method1(){
+        System.out.println("method1");
+    }
+
+    // 2) 매개 변수O, 리턴X
+    public void method2(int x){
+        System.out.println("method2");
+    }
+
+    // 3) 매개 변수X, 리턴o
+    public int method3(){
+        System.out.println("method3");
+        return 10;
+    }
+
+    // 4) 매개 변수O, 리턴O
+    public int method4(int y){
+        System.out.println(y + "를 이용한 method4");
+        return y;
+    }
+
+    public static void main(String[] args) {
+        Method_10 m1 = new Method_10();
+
+        m1.method1();
+        m1.method2(10);
+
+        int x = m1.method3();
+        System.out.printf("method3이 리턴한 %d\n", x);
+
+        int x2 = m1.method4(50);
+        System.out.println(x2);
+    }
+
+
 }
---
+++ prjTest/src/prjTest/StringMethod_10.java
@@ -0,0 +1,20 @@
+package prjTest;
+
+public class StringMethod_10 {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str.length());
+
+        String str1 = new String("hello");
+
+        // 문자열 붙이기 - concat
+        System.out.println(str1.concat(" world"));  // 문자열 합치기 => hello world
+        System.out.println(str1);       // hello
+
+        // 문자열 자르기 - subString
+        System.out.println(str1.substring(1, 3));   // index 1~3번까지의 결과
+        System.out.println(str1.substring(2));      // index 2에서 마지막까지의 결과
+
+    }
+}
