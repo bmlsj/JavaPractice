01a4e230447fd5d53124dd83bcf37e85332860fc
bmlsj <gaga2353@gmail.com>
2022-08-03 11:50:24 +0900

    Class

--- prjTest/src/prjTest/class_test/Car.java
+++ prjTest/src/prjTest/class_test/Car.java
@@ -1,2 +1,8 @@
-package prjTest.class_test;public class Car {
+package prjTest.class_test;
+
+public class Car {
+    
+    // 클래스란?
+    // 객체를 만들기 위한 틀
+
 }
--- prjTest/src/prjTest/class_test/CarExam.java
+++ prjTest/src/prjTest/class_test/CarExam.java
@@ -1,2 +1,13 @@
-package prjTest.class_test;public class CarExam {
+package prjTest.class_test;
+
+public class CarExam {
+
+    public static void main(String[] args) {
+
+        // 클래스는 참조형 변수
+        // 변수가 인스턴스를 가리킴(참조)
+
+        Car c1 = new Car();         // new를 통해 만든 객체 = 인스턴스
+        Car c2 = new Car();
+    }
 }
