88fad0053fc8f3c2805a071eabe868460be11616
bmlsj <gaga2353@gmail.com>
2022-08-03 11:50:03 +0900

    forEach

--- prjTest/src/prjTest/forEach_8.java
+++ prjTest/src/prjTest/forEach_8.java
@@ -1,2 +1,20 @@
-package prjTest;public class forEach_8 {
+package prjTest;
+
+public class forEach_8 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // 기존 배열 출력
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+        int[] arr2 = {10, 20, 30, 40, 50};
+        // forEach 구문
+        for(int i:arr2){
+            System.out.println(i);
+        }
+    }
 }
