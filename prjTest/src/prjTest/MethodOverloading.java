631217c5961eb27aea226d42f836652085475086
bmlsj <gaga2353@gmail.com>
2022-08-03 15:34:16 +0900

    15. 메소드 오버로딩

--- prjTest/src/prjTest/MethodOverloading.java
+++ prjTest/src/prjTest/MethodOverloading.java
@@ -5,16 +5,16 @@ public class MethodOverloading {
     // 15. 메소드 오버로딩
     // 이름은 같지만 매개변수가 다른 메소드 생성 가능
 
-    public int plus(int x, int y){
-        return x+y;
+    public int plus(int x, int y) {
+        return x + y;
     }
 
-    public int plus(int x, int y, int z){
-        return x+y+z;
+    public int plus(int x, int y, int z) {
+        return x + y + z;
     }
 
-    public String plus(String x, String y){
-        return x+y;
+    public String plus(String x, String y) {
+        return x + y;
     }
 
     public static void main(String[] args) {
@@ -22,6 +22,6 @@ public class MethodOverloading {
 
         System.out.println(m.plus(5, 10));
         System.out.println(m.plus(1, 2, 3));
-        System.out.println(m.plus("hello"," world"));
+        System.out.println(m.plus("hello", " world"));
     }
 }
